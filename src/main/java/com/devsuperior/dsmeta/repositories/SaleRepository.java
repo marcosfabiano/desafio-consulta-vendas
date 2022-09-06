c19457e8e8fad4c747d920969fdc3ac0591912fc
Marcos Fabiano <marcos.fabiano67@outlook.com>
2022-09-05 23:49:30 -0300

    Challenge concluded

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,26 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleReportDTO(obj.id, obj.date, obj.amount, obj.seller.name) "
+            + "FROM Sale obj "
+            + "WHERE obj.date BETWEEN :minD AND :maxD "
+            + "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))" )
+    Page<SaleReportDTO> searchReport(LocalDate minD, LocalDate maxD, String name, Pageable pageable);
 
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) "
+            + "FROM Sale obj "
+            + "WHERE obj.date BETWEEN :minD AND :maxD "
+            + "GROUP BY obj.seller.name" )
+    List<SaleSummaryDTO> searchSummary(LocalDate minD, LocalDate maxD);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,13 +1,21 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SaleService {
@@ -19,5 +27,37 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleReportDTO> findReport(String minDate, String maxDate, String name, Pageable pageable) {
+		LocalDate maxD;
+		LocalDate minD;
+		try {
+			maxD = LocalDate.parse(maxDate);
+		} catch (DateTimeParseException e) {
+			maxD = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		}
+		try {
+			minD = LocalDate.parse(minDate);
+		} catch (DateTimeParseException e) {
+			minD = maxD.minusYears(1L);
+		}
+		return repository.searchReport(minD, maxD, name, pageable);
+	}
+
+	public List<SaleSummaryDTO> findSummary(String minDate, String maxDate) {
+		LocalDate maxD;
+		LocalDate minD;
+		try {
+			maxD = LocalDate.parse(maxDate);
+		} catch (DateTimeParseException e) {
+			maxD = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		}
+		try {
+			minD = LocalDate.parse(minDate);
+		} catch (DateTimeParseException e) {
+			minD = maxD.minusYears(1L);
+		}
+		return repository.searchSummary(minD, maxD);
 	}
 }
